29e47082ebb70b4ccfcf23c9bf10cc3834c1ea35
8264475 <8264475@gmail.com>
2022-08-31 16:34:01 +0800

    20220831

--- JavaLab/src/Employee.java
+++
@@ -1,5 +0,0 @@
-
-public class Employee {
-	int empno;//員工編號
-	String name;//員工姓名, ex."Hello World"
-}
--- JavaLab/src/array/Employee.java
+++
@@ -1,6 +0,0 @@
-package array;
-
-public class Employee {
-	int empno;//員工編號
-	String name;//員工姓名, ex."Hello World"
-}
